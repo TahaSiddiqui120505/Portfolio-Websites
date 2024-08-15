e7421fd20b041d8e835e85ef12bb4015a19b7ce5
Taha Siddiqui <siddiquitaha120@gmail.com>
2024-08-15 11:26:21 +0530

    Loader done

--- Portfolio-01/script.js
+++ Portfolio-01/script.js
@@ -1,22 +1,38 @@
+function revealToSpan(){
+    document.querySelectorAll(".reveal")
+.forEach(function(elem){
+    //creatre two spans
+    var parent = document.createElement("span");
+    var child = document.createElement("span");
+
+    //parent and child both set their respective classes
+    parent.classList.add("parent");
+    child.classList.add("child");
+
+    //span parent gets child and child gets elem details
+    child.innerHTML = elem.innerHTML;
+    parent.appendChild(child);
+
+    //elem replaces its value with parent span
+    elem.innerHTML = "";
+    elem.appendChild(parent);
+});
+}
+
+revealToSpan();
+
 var tl = gsap.timeline();
 
 tl
-    .to("#fs",{
-        height: 0,
-        duration: 2,
-        ease: Expo.easeInOut
+    .from(".child span",{
+        x: 100,
+        delay: 1,
+        duration: 1,
+        ease: Circ.easeInOut
     })
 
-    .to("#elem",{
-        height: "100%",
-        duration: 2,
-        delay: -2,
-        ease: Expo.easeInOut
-    })
-
-    .to("#white",{
-        height: "100%",
-        duration: 2,
-        delay: -1.6,
-        ease: Expo.easeInOut
-    })
+    .to(".parent .chlild",{
+        y: "-100%",
+        duration: 1,
+        ease: Circ.easeInOut
+})
